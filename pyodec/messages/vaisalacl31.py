2aa7a61f7529f526cb1a74ccf9a83a62b6daf59a
Joe <joeyoungwx@gmail.com>
2014-07-06 23:53:02 -0500

    tweaked the default CL-31 decoder

--- pyodec/messages/vaisalacl31.py
+++ pyodec/messages/vaisalacl31.py
@@ -7,7 +7,7 @@ class cl31Dm2(MessageDecoder):
         OB_LENGTH = 770  # FIXME - the current return length is limited to 770
         SCALING_FACTOR = 1.0e9
         
-        'break the full ob text into it\'s constituent parts'
+        #break the full ob text into it's constituent parts
         p1 = message.split(unichr(002))
         p2 = p1[1].split(unichr(003))
         code = p1[0].strip()
@@ -16,9 +16,9 @@ class cl31Dm2(MessageDecoder):
     
         data = ob.split("\n")  # split into lines
     
-        'the last line of the profile should be the data line'
+        #the last line of the profile should be the data line'
         prof = data[-1].strip()
-        'grab status lines'
+        #grab status lines'
         sl1 = data[0].strip()
         sl2 = data[-2].strip()  # I will skip any intermediate data lines...
     
@@ -26,11 +26,11 @@ class cl31Dm2(MessageDecoder):
                         sl1[1].replace('A', '2').replace('W', '1')] +
                         sl1[2:-13].replace('/', '0').split() + sl2[:-14].split(),
                         dtype=np.float32)
-        'status should have a length of 13... we shall see...'
+        #status should have a length of 13... we shall see...'
         # determine height difference by reading the last digit of the code
         height_codes = [0, 10, 20, 5, 5]  # '0' is not a valid key, and will not happen
         data_lengths = [0, 770, 385, 1500, 770]
-        'length between 770 and 1500'
+        #length between 770 and 1500'
         datLen = data_lengths[int(code[-1])]
         htMult = height_codes[int(code[-1])]
         values = np.zeros(datLen, dtype=np.float32)
@@ -66,5 +66,5 @@ vvars.addvar('STATUS','CL-31 Status information','float32',(13,),'Null')
 fvars = FixedVariableList()
 fvars.addvar('HEIGHT','m AGL','int',np.arange(770)*10)
 
-decoder = cl31Dm2(vars=vvars,fixed_vars=fvars)
+decoder = cl31Dm2(vars=vvars,fixed_vars=fvars) 
 
